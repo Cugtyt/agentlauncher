ecccdd14e1c7018750a55041f979ae291016c510
Xin Li (from Dev Box) <xinli4@microsoft.com>
2025-09-25 11:42:13 +0800

    fix import

--- agentlauncher/llm_interface/handler.py
+++ agentlauncher/llm_interface/handler.py
@@ -1,8 +1,6 @@
 from collections.abc import Awaitable, Callable
-from typing import TYPE_CHECKING
 
-if TYPE_CHECKING:
-    from agentlauncher.events import EventBus
+from agentlauncher.eventbus import EventBus
 
 from .message import (
     RequestMessageList,
@@ -15,7 +13,7 @@ type LLMHandler = Callable[
         RequestMessageList,
         RequestToolList,
         str,  # agent_id
-        "EventBus",
+        EventBus,
     ],
     Awaitable[ResponseMessageList] | ResponseMessageList,
 ]
